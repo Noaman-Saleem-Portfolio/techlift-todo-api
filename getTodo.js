5e60b21a9605315b0c5272380c9fd54b5958ea71
Noaman Saleem <osamaarshed11@gmail.com>
2022-11-12 16:44:15 +0500

    generated dynamic li items

--- getTodo.js
+++ getTodo.js
@@ -4,9 +4,12 @@
 // Get All todos
 let getIdInput = document.getElementById("getIdInput");
 let getIdBtn = document.getElementById("getIdBtn");
+let todos = document.getElementById("todos");
+let ulTodo = document.getElementById("ulTodo");
 
 // console.log(getIdInput);
 // console.log(getIdBtn);
+// console.log(todos);
 
 getIdBtn.addEventListener("click", () => {
   let userId = getIdInput.value;
@@ -22,7 +25,17 @@ getIdBtn.addEventListener("click", () => {
       // console.log(response);
       return response.json();
     })
-    .then((data) => console.log(data))
+    .then((data) => {
+      for (let i = 0; i < data.todos.length; i++) {
+        let li = document.createElement("li");
+        ulTodo.appendChild(li);
+        li.innerHTML = data.todos[i].title;
+      }
+
+      //   console.log("Created");
+      //   console.log(li);
+      console.log(data.todos[0].title);
+    })
     .catch((error) => {
       console.log(`Failed to fetch: ${error}`);
     });
